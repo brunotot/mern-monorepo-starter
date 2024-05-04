f862969d672396ebaf26679db2a05f209b48435a
Bruno Tot <brunotot10000@gmail.com>
2024-05-04 13:31:24 +0200

    chore: add markdown for dependencies

---
+++ md/dependencies/dependencies.md
@@ -0,0 +1,72 @@
+# backend
+
+| Name | Version | Description |
+|------|---------|-------------|
+| @anatine/zod-openapi | ^2.2.5 | Testings |
+| @org/shared | file:../shared/src | - |
+| @ts-rest/core | ^3.45.0 | - |
+| @ts-rest/express | ^3.45.0 | - |
+| @ts-rest/open-api | ^3.45.0 | - |
+| @tsvdec/core | ^2.0.11 | - |
+| @tsvdec/decorators | ^1.0.7 | - |
+| bcrypt | ^5.1.1 | - |
+| body-parser | ^1.20.2 | - |
+| bottlejs | ^2.0.1 | - |
+| compression | ^1.7.4 | - |
+| cookie-parser | ^1.4.6 | - |
+| cors | ^2.8.5 | - |
+| cross-dirname | ^0.1.0 | - |
+| dotenv | ^16.4.5 | - |
+| express | ^4.18.2 | - |
+| express-rate-limit | ^7.2.0 | - |
+| helmet | ^7.1.0 | - |
+| hpp | ^0.2.3 | - |
+| http-status | ^1.7.4 | - |
+| jsonwebtoken | ^9.0.2 | - |
+| mongodb | ^6.5.0 | - |
+| mongoose | ^8.3.2 | - |
+| morgan | ^1.10.0 | - |
+| openapi3-ts | ^4.3.1 | - |
+| swagger-jsdoc | ^6.2.8 | - |
+| swagger-ui-express | ^5.0.0 | - |
+| winston | ^3.11.0 | - |
+| winston-daily-rotate-file | ^5.0.0 | - |
+| zod | ^3.22.5 | - |
+
+# frontend
+
+| Name | Version | Description |
+|------|---------|-------------|
+| @emotion/react | ^11.11.4 | abece iasxko asdhuashdi ash |
+| @emotion/styled | ^11.11.0 | - |
+| @mui/icons-material | ^5.15.14 | - |
+| @mui/lab | 5.0.0-alpha.169 | - |
+| @mui/material | ^5.15.14 | - |
+| @mui/styles | ^5.15.14 | - |
+| @mui/x-tree-view | ^7.0.0 | - |
+| @org/shared | file:../shared/src | - |
+| @preact/signals-react | ^2.0.1 | - |
+| @tanstack/react-query | ^5.32.1 | - |
+| @ts-rest/core | ^3.45.0 | - |
+| @ts-rest/react-query | ^3.45.0 | - |
+| @uidotdev/usehooks | ^2.4.1 | - |
+| axios | ^1.6.8 | - |
+| i18next | ^23.10.1 | - |
+| i18next-browser-languagedetector | ^7.2.0 | - |
+| i18next-http-backend | ^2.5.0 | - |
+| material-ui-popup-state | ^5.1.0 | - |
+| react | ^18.2.0 | - |
+| react-dom | ^18.2.0 | - |
+| react-i18next | ^14.1.0 | - |
+| react-router-dom | ^6.22.3 | - |
+
+# shared
+
+| Name | Version | Description |
+|------|---------|-------------|
+| @anatine/zod-openapi | ^2.2.5 | Testings |
+| @ts-rest/core | ^3.45.0 | - |
+| @ts-rest/open-api | ^3.45.0 | - |
+| http-status | ^1.7.4 | - |
+| zod | ^3.22.5 | - |
+
---
+++ md/dependencies/updateDepDocs.js
@@ -0,0 +1,161 @@
+import fs from "fs";
+import path from "path";
+import { getDirname } from "cross-dirname";
+import { exec } from "child_process";
+
+const PACKAGES_DIR_PATH = "./../../packages";
+
+const DESCRIPTIONS_FILE_NAME = "dependencyDescriptions.json";
+const PACKAGE_JSON_FILE_NAME = "package.json";
+const DEPENDENCIES_MD_FILE_NAME = "dependencies.md";
+
+function pathFromDir(...paths) {
+  return path.join(getDirname(), ...paths);
+}
+
+function runPrettierOnFile(filePath) {
+  exec(`npx prettier --write ${filePath}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error running Prettier: ${error}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Prettier stderr: ${stderr}`);
+    }
+    //console.log("File formatted with Prettier!");
+  });
+}
+
+function readJsonFile(filePath) {
+  try {
+    let json = {};
+    if (fs.existsSync(filePath)) {
+      json = JSON.parse(fs.readFileSync(filePath));
+    }
+    return json;
+  } catch (err) {
+    console.error("Error reading JSON file:", err);
+    return {};
+  }
+}
+
+function readDependencies(packagePath) {
+  try {
+    const { dependencies } = JSON.parse(fs.readFileSync(packagePath));
+    return dependencies || {};
+  } catch (error) {
+    console.error(`Error reading dependencies from ${packagePath}:`, error);
+    return {};
+  }
+}
+
+function sortObjectKeys(obj) {
+  const keys = Object.keys(obj);
+  keys.sort();
+  const sortedObject = {};
+  for (const key of keys) {
+    sortedObject[key] = obj[key];
+  }
+  return sortedObject;
+}
+
+function getDependenciesFromPackages() {
+  const packageFolders = fs.readdirSync(pathFromDir(PACKAGES_DIR_PATH));
+  const dependenciesByFolder = {};
+
+  for (const packageFolder of packageFolders) {
+    const packagePath = pathFromDir(PACKAGES_DIR_PATH, packageFolder, PACKAGE_JSON_FILE_NAME);
+    const dependencies = readDependencies(packagePath);
+    dependenciesByFolder[packageFolder] = dependencies;
+  }
+
+  return dependenciesByFolder;
+}
+
+function updateDependencyDescriptions(dependencies) {
+  try {
+    let dependencyDescriptions = readJsonFile(pathFromDir(DESCRIPTIONS_FILE_NAME));
+
+    for (const [dependency, description] of Object.entries(dependencies)) {
+      if (!dependencyDescriptions.hasOwnProperty(dependency)) {
+        dependencyDescriptions[dependency] = "-";
+      }
+    }
+
+    fs.writeFileSync(
+      pathFromDir(DESCRIPTIONS_FILE_NAME),
+      JSON.stringify(sortObjectKeys(dependencyDescriptions), null, 2),
+    );
+    //console.log("dependencyDescriptions.json updated successfully!");
+  } catch (error) {
+    console.error("Error updating dependencyDescriptions.json:", error);
+  }
+}
+
+function updateDescriptionsJson() {
+  const packageNames = fs.readdirSync(pathFromDir(PACKAGES_DIR_PATH));
+
+  for (const packageName of packageNames) {
+    const packagePath = pathFromDir(PACKAGES_DIR_PATH, packageName, PACKAGE_JSON_FILE_NAME);
+    const dependencies = readDependencies(packagePath);
+    updateDependencyDescriptions(dependencies);
+  }
+}
+
+function generateMarkdownTables(packageDependencies, packageDescriptions) {
+  const markdownTables = {};
+
+  for (const [packageName, dependencies] of Object.entries(packageDependencies)) {
+    const tableRows = Object.entries(dependencies).map(([dependencyName, version]) => {
+      const description = packageDescriptions[dependencyName];
+      return `| ${dependencyName} | ${version} | ${description} |`;
+    });
+
+    const tableHeader = `| Name | Version | Description |\n|------|---------|-------------|`;
+    const markdownTable = `${tableHeader}\n${tableRows.join("\n")}`;
+
+    markdownTables[packageName] = markdownTable;
+  }
+
+  return markdownTables;
+}
+
+function writeToDependenciesMd(markdownTables) {
+  try {
+    const stream = fs.createWriteStream(pathFromDir(DEPENDENCIES_MD_FILE_NAME));
+
+    for (const [packageName, markdownTable] of Object.entries(markdownTables)) {
+      stream.write(`# ${packageName}\n\n`);
+      stream.write(markdownTable + "\n\n");
+    }
+
+    stream.end();
+    //console.log("Dependencies markdown file generated successfully!");
+  } catch (error) {
+    console.error("Error writing dependencies markdown file:", error);
+  }
+}
+
+function addFileToGit(filePath) {
+  exec(`git add ${filePath}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error adding file to Git: ${error}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Git add stderr: ${stderr}`);
+    }
+  });
+}
+
+function main() {
+  updateDescriptionsJson();
+  const packageDescriptions = readJsonFile(pathFromDir(DESCRIPTIONS_FILE_NAME));
+  const packageDependencies = getDependenciesFromPackages();
+  const markdownTables = generateMarkdownTables(packageDependencies, packageDescriptions);
+  writeToDependenciesMd(markdownTables);
+  runPrettierOnFile(pathFromDir(DEPENDENCIES_MD_FILE_NAME));
+  addFileToGit(pathFromDir(DEPENDENCIES_MD_FILE_NAME));
+}
+
+main();
