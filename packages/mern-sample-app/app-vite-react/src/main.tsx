4b5d1c28729addde3b9287bd2a88189da54eb362
Bruno Tot <brunotot10000@gmail.com>
2024-09-08 18:56:23 +0200

    chore: add import for i18next init fn in main.tsx

--- packages/mern-sample-app/app-vite-react/src/main.tsx
+++ packages/mern-sample-app/app-vite-react/src/main.tsx
@@ -1,6 +1,8 @@
+import "@org/app-vite-react/main.css";
+import "@org/app-vite-react/lib/i18next/i18n";
+
 import { reactServer } from "@org/app-vite-react/server";
 import { routes } from "@org/app-vite-react/app/routes";
-import "@org/app-vite-react/main.css";
 
 reactServer.run({
   routes,
