55cb29300c466a02b15fe140550c30c483d71e78
Bruno Tot <brunotot10000@gmail.com>
2024-10-01 22:35:26 +0200

    chore: fix tests

--- packages/mern-sample-app/app-node-express/test/__mocks__/AuthorizationRepositoryMock.ts
+++ packages/mern-sample-app/app-node-express/test/__mocks__/AuthorizationRepositoryMock.ts
@@ -2,18 +2,25 @@ import type { AuthorizationRepository } from "../../dist/infrastructure/reposito
 import type { ApiKeycloakUser } from "@org/lib-api-client";
 
 export class AuthorizationRepositoryMock implements AuthorizationRepository {
+  static roles: Record<string, string[]> = {
+    admin: ["admin", "user"],
+    user: ["user"],
+  };
+
   static users: ApiKeycloakUser[] = [
     {
       id: "1",
       username: "admin",
-      realmRoles: ["admin"],
     },
     {
       id: "2",
       username: "user",
-      realmRoles: ["user"],
     },
   ];
+
+  async findRolesByUserId(userId: string): Promise<string[]> {
+    return await Promise.resolve(AuthorizationRepositoryMock.roles[userId] ?? []);
+  }
 
   async findUserByUsername(username: string): Promise<ApiKeycloakUser | null> {
     return await Promise.resolve(
